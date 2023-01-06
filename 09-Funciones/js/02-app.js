660ed304756e20bb1bbd55095e25f9ef54fa912f
StevenCiri <stevenandrew.saca@gmail.com>
2023-01-06 18:15:35 -0500

    09-Funciones Finish

---
+++ 09-Funciones/js/02-app.js
@@ -0,0 +1,14 @@
+// // JavaScript lo lee sin problemas
+// Declaración de función ( Function Declaration )
+sumar();
+function sumar() {
+    console.log(2 + 2);
+}
+
+
+// // A este no :v
+// Expresión de Función (Function  Expression)
+sumar2();
+const sumar2 = function() {
+    console.log(3 + 3);
+}
